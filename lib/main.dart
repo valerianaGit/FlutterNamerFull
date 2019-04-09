47578439bcb6b66b691f7a3bc12ddf2709bbb6b2
Valeriana <valeria.do@gmail.com>
2019-04-09 12:08:54 -0500

    Added a color theme to the app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
    // final wordPair = WordPair.random();
     return MaterialApp(
   title: "Startup Name Generator",
+  theme: new ThemeData(
+    primaryColor: Colors.cyan,
+  ),
   home: RandomWords(),  
     );
   }
